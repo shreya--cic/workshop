aa6b13294c1e09b95bb884811b5da53d0244a93a
unknown <shikharcic23@gmail.com>
2013-10-19 15:23:03 +0530

    reset

--- cube_volume.py
+++ cube_volume.py
@@ -2,7 +2,7 @@ def cube_volume():
     l=int(raw_input('Enter the side'))
     volume=l*l*l
     print 'volume is'
-
+    print 'change to study reset'
     return volume
 
 
--- index.py
+++ index.py
@@ -2,14 +2,25 @@ def main():
 pass
 
 def cube_volume():
-pass
+	l=int(raw_input('enter the side'))
+	volume=l*l*l
+	print 'volume is '
+	print volume
+	return volume
 
 def cube_surface_area():
-pass
+	pass
 
 def cuboid_volume():
-pass
+	l=int(raw_input('enter the length '))
+	b=int(raw_input('Enter the breadth'))
+	h=int(raw_input('Enter the height '))
+	cuboid_volume=l*b*h
+	print 'volume is'
+	print cuboid_volume
+	return cuboid_volume
+        
 
 def cuboid_surface_area();
-pass
+	pass
 
